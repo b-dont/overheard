edbab0fff3b00f09ecd18b843ede819b5e873b75
Brandon Phillips <btp@brandont.dev>
2024-08-01 16:27:15 -0600

    Nobot check switch case

--- src/index.js
+++ src/index.js
@@ -9,18 +9,21 @@ const subscribe = async() => {
   console.log("Listening for mentions..");
 
   for await (const event of stream.user.notification.subscribe()) {
-    switch (event.event) {
 
+    switch (event.event) {
       case "notification": {
         if (event.payload.status.inReplyToId !== 'undefined') {
+
           console.log("Found mention", event.payload.status.id);
           var parent = toString(event.payload.status.inReplyToAccountId);
           var parentUser = getParentUser(parent);
           console.log(`Parent user: ${parentUser.user}`);
+
           if (!mentions.checkNobot(event)) {
             reblogParent(event.payload.status.inReplyToId, connection);
             break;
           }
+
         } else {
             console.log("User has #nobot in bio, abstaining from reblog.");
             break
